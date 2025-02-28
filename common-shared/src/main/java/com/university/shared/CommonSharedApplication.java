40ac5306ee733110ff994fc6ed0f8ac8ea6b48c7
sanjukta <sanjuktakar.diya@gmail.com>
2025-02-28 21:58:28 +0530

    First commit

---
+++ common-shared/src/main/java/com/university/shared/CommonSharedApplication.java
@@ -0,0 +1,13 @@
+package com.university.shared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonSharedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonSharedApplication.class, args);
+	}
+
+}
